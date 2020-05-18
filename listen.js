4a3e2ceaa8af45e2766032ce0645ccd958bd9433
James Cowell <james.cowell28@gmail.com>
2020-05-18 18:15:23 +0100

    added apiRouter and rallyRouter

---
+++ listen.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(9090, () => {
+  console.log('listening at port 9090...');
+});
---
+++ routes/rallyRouter.js
@@ -0,0 +1,5 @@
+const rallyRouter = require('express').Router();
+
+rallyRouter.get('/', getRallies);
+
+module.exports = rallyRouter;
